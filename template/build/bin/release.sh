25f1618c3b2815e38100261dac852f33a9595ec8
cuijj <jiajie.cui@midea.com.cn>
2018-06-13 10:20:08 +0800

    自动发布时提交代码免验证

--- template/build/bin/release.sh
+++ template/build/bin/release.sh
@@ -17,15 +17,15 @@ npm run build
 # 提交dist
 git add -A
 git add -f dist/*.*
-git commit -m "build: build $VERSION"
+git commit --no-verify -m "build: build $VERSION"
 
 # 更新版号本并提交
-npm version $VERSION --message "build: release $VERSION"
+npm version $VERSION --no-verify --message "build: release $VERSION"
 
 # 生成changelog并提交
 npm run gen:changelog
 git add -A
-git commit -m "docs: change log $VERSION"
+git commit --no-verify -m "docs: change log $VERSION"
 
 # 推送远程
 git push origin refs/tags/v$VERSION
